a3c15cd6778aba68e4745b9bfe3dbe0dc770c4f7
cabralpriscila <priscilacabral.dev@gmail.com>
2022-12-20 22:04:00 -0300

    redirect index page to ideas page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
+  root to: redirect('/ideas')
   resources :ideas
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
